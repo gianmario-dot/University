e3d84a4c937812f0a5b1cb2a859d96ceea88f5c7
Gianmario Pelanda <gianmariopela2004@gmail.com>
2026-05-21 12:19:14 +0200

    Analizzatore immagini completato

--- Analisi Immagini/Analisi_00.py
+++ Analisi Immagini/Analisi_00.py
@@ -1,4 +1,8 @@
 import numpy as np
+
+import matplotlib
+matplotlib.use('TkAgg')
+
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import tkinter as tk
@@ -14,10 +18,73 @@ def chiudi_tutto(_):
 
 
 
-def analisi_immagine(soglia,imm,mappa):
+def analisi_immagine(soglia,imm,mappa, real_time):
+
+    #Sub routine che controlla se nell'intorno di un punto interessante è stato già verificato se no lo senga da vedere
+    def segna_intorno(a, b):
+        nonlocal stack
+
+        for m in range(a-1, a+2):
+            for n in range(b-1, b+2):
+                if imm[m, n, 1]==0:
+                    stack.append([m,n])     # Se non visto aggiungo
 
 
-    return
+
+    # Scansione su tutte le righe e su tutte le colonne
+    h,w,_=imm.shape          # Categorizzo così altezza (h), larghezza (w) e canali (c)
+
+    # Creo matricce che analizza se punti già analizzati e se sono sopra o sotto soglia
+    # Distruggo la vecchia immagine e riempo di zeri nei canali blu e verde SOLO PERCHE IN SCALA GRIGI NON FUNZIONA SE A COLORI
+    imm[:,:,1]=0      # Usato?
+    imm[:,:,1]=0      # Sopra o sotto soglia?
+
+    # Matrici aree e stack
+    area=[]
+    stack=[]
+
+    # Faccio scansione
+    for i in range(1, w):
+        for j in range(1, h):
+
+            # Verifico se già stato usato
+            if imm[i, j, 1]==0:
+
+                # Continuo analisi
+                # Dico che usato
+                imm[i,j,1]=0.5      # Cambio il colore così da veirficarlo
+                
+                if imm[i, j, 0]> soglia:
+                    imm[i,j,2]=1        # Rendo colorata l'area così da vederla graficamente
+                    
+                    # Aggiungo alla lista area la nuova area da contare
+                    area.append(1)
+                    
+                    # Sistema controllo dei vicini
+                    segna_intorno(i, j)
+
+                    while len(stack)>0:         # Continuo fino a quando non ho finito i vicini LIFO
+                        n=len(stack)
+                        x,y=stack[n-1][:]       # Estrai coordinate puntii ultimo posto stack
+                        stack.pop()             # Elimino utlimo elemento
+
+                        imm[x, y, 1]=0.5        # Segno il punto come usato
+
+                        # Controllo se sopra sotto soglia
+                        if imm[x, y, 0] > soglia:
+                            imm[x,y, 2]=1       # Cambio colore per vederlo
+
+                            area[-1]+=1         # Incrementa area
+
+                            segna_intorno(x, y)
+
+        if real_time=='y':
+            mappa.set_data(imm)                     # Aggiornamento grafico per vederlo poi tolgo per efficenze
+            plt.pause(0.001)
+
+        mappa.set_data(imm)
+
+    return area
 
 
 
@@ -72,8 +139,11 @@ def main():
 
 
     soglia=float(input('Valore della soglia (0-1)= '))
+    real_time=input('Mostra scansione in tempo reale? (y/n)')
 
-    risultati=analisi_immagine(soglia,img_con_bordo,mappa)
+
+
+    risultati=analisi_immagine(soglia,img_con_bordo,mappa, real_time)
 
     grafico2.hist(risultati, bins=10, color='skyblue', edgecolor='black')
 
